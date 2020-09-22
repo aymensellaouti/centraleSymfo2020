854a02fe63ccba9a6a32455195053a6582cc9f18
aymen <aymen.sellaouti@gmail.com>
2020-09-22 10:10:04 +0100

    twig examples

--- src/Controller/TwigController.php
+++ src/Controller/TwigController.php
@@ -8,16 +8,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class TwigController extends AbstractController
 {
     /**
-     * @Route("/twig", name="twig")
+     * @Route(
+     *     "/twig/{nb?5<\d+>}",
+     *      name="twig"
+     * )
      */
-    public function index()
+    public function index($nb)
     {
-        $name="aymen";
-        $tab = [10,20,3];
+        $tab = [];
+        for ($i = 0; $i < $nb; $i++) {
+            $tab[$i] = random_int(1, 1000);
+        }
         return $this->render('twig/index.html.twig', [
-            'nom' => $name,
-            'age' => '38',
-            'monTableau' => $tab
+            'monTableauDentier' => $tab
+        ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/colortab", name="twig.colorTab")
+     */
+    public function tabColor()
+    {
+        $users = [
+            ['name' => 'sellaouti', 'firstname' => 'aymen', 'age' => 38],
+            ['name' => 'ben slimen', 'firstname' => 'ahmed', 'age' => 38],
+            ['name' => 'ben salah', 'firstname' => 'mohamed', 'age' => 38]
+        ];
+        return $this->render('twig/colorTab.html.twig', [
+           'colorTab' => $users
         ]);
     }
 }
